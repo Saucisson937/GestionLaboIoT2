d6e7a07f602d0a4308d00b295b76b79249f24b76
Jo <jordan.rosemond@gmail.com>
2018-02-26 10:09:08 +0100

    Add models (user and item) + student mail conditions V1 + login/password condition V1

---
+++ GestionLaboIot/GestionLaboIot/Models/Items.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionLaboIot.Models
+{
+    class Items
+    {
+		public int Id { get; set; }
+		public string Nom { get; set; }
+		public string Categorie { get; set; }
+		public string SousCategorie { get; set; }
+		public string Quantite { get; set; }
+		public string QRCode { get; set; }
+	}
+}
--- GestionLaboIot/GestionLaboIot/Pages/StudentMail.xaml.cs
+++ GestionLaboIot/GestionLaboIot/Pages/StudentMail.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -20,7 +22,35 @@ namespace GestionLaboIot.Pages
 
 		private async void Button_ValidMail_ClickedAsync(object sender, EventArgs e)
 		{
-			await Navigation.PushModalAsync(new StudentChoice());
+			if(!(String.IsNullOrEmpty(entry_Mail.Text)))//test si pas null ou pas vide
+			{
+				entry_Mail.Text = entry_Mail.Text.Replace(" ", "").ToLower();// enleve les espaces blancs et met en minuscule
+
+				if(Regex.IsMatch(entry_Mail.Text, @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+				@"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$")) //test si format mail valide
+				{
+					if (entry_Mail.Text.EndsWith("ynov.com")) //test si domaine = ynov.com
+					{
+						await Navigation.PushModalAsync(new StudentChoice());
+					}
+					else
+					{
+						await DisplayAlert("Attention", "Veuillez indiquer une adresse mail ynov", "Ok");
+						entry_Mail.Text = "";
+					}
+				}
+				else
+				{
+					await DisplayAlert("Attention", "Format de mail invalide", "Ok");
+					entry_Mail.Text = "";
+				}				
+			}
+			else
+			{
+				await DisplayAlert("Attention", "Veuillez indiquer une adresse mail ynov", "Ok");
+			}
+			
+			
 		}
 	}
 }
